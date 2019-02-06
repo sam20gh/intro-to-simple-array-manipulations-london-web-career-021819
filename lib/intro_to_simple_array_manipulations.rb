52213e224e54b6bc4084f134c15d56ad2d736cc4
sam ghoreishi <sam20gh@gmail.com>
2019-02-06 22:54:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,7 +16,6 @@ def using_pop(array)
   
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   great_hits_of_the_nineties.pop
-  great_hits_of_the_nineties =[]
 end
 
 def pop_with_args(array, string)
