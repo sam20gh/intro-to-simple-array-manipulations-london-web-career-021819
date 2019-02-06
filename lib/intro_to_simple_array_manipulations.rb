84c17486a73ece2c8b4fec466abe4f52bf112c72
sam ghoreishi <sam20gh@gmail.com>
2019-02-06 22:51:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -12,7 +12,7 @@ def using_unshift(array, string)
 end
 
 
-def using_pop(array, string)
+def using_pop(array)
   
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   great_hits_of_the_nineties.pop
